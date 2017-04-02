1a98dd1ed973c7d3f20b2721d9bc2bdf317e6eec
callum brennan <callum.brennan@hotmail.co.uk>
2017-04-02 23:13:55 +0100

    Table view of emojis,
    the table view now looks for the emoji array to know how many rows there are and what the cells consist of

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource , UITableViewDelegate
+{
 
+    @IBOutlet weak var tabelview: UITableView!
+    
+    
+    var emojis = ["😀","😋","😍","🖕🏻","🦁","🌕","🎈","☺️","😖","😻"]
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tabelview.dataSource = self
+        tabelview.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return  emojis.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        print(indexPath.row)
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+        
     }
 
     override func didReceiveMemoryWarning() {
